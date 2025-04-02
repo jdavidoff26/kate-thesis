07bc3b968a476caae46ae438749bcfdcdc01f1bb
JJ <jdavidoff26@uchicago.edu>
2025-04-02 16:54:14 -0500

    data-script created

---
+++ data-script.R
@@ -0,0 +1,10 @@
+library(tidyverse)  # This loads readr, dplyr, tidyr, tibble, ggplot2, etc.
+library(DT)         # For interactive HTML tables
+
+generalHarris <- read_csv("~/Downloads/General_Harris.csv")
+generalTrump <- read_csv("~/Downloads/General_Trump.csv")
+officialHarris <- read_csv("~/Downloads/Official_Harris.csv")
+officialTrump <- read_csv("~/Downloads/Official_Trump.csv")
+
+
+
